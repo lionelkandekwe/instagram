573f6b7aee31555e10c888fda48bc0fa8443050b
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-04-05 16:02:03 +0200

    add navigation Screen

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import HomeScreen from "./screens/HomeScreen"
-import NewPostScreen from "./screens/NewPostScreen"
+import SignedInStack from "./navigation"
 
 export default function App() {
-  return <NewPostScreen />
+  return <SignedInStack />
 }
