d1b0b265c4cba5cc4488d2184394f8ef90248352
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-04-08 09:45:58 +0200

    ignore react-native-gesture-handler logs

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import SignedInStack from "./navigation"
 
+import { LogBox } from "react-native"
+
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+])
+
 export default function App() {
   return <SignedInStack />
 }
