d997b862f219e77de81dddea91de2b05e3ba540c
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-04-08 10:07:40 +0200

    Share Post Go back HomePage

--- components/newPost/FormikPostUploader.js
+++ components/newPost/FormikPostUploader.js
@@ -12,12 +12,16 @@ const uploadPostSchema = Yup.object().shape({
   caption: Yup.string().max(2200, "Caption is too long"),
 })
 
-const FormikPostUploader = () => {
+const FormikPostUploader = ({ navigation }) => {
   const [thumbnailUrl, setThumbnailUrl] = useState(PLACEHOLDER_IMAGE)
   return (
     <Formik
       initialValues={{ caption: "", imageUrl: "" }}
-      onSubmit={(values) => console.log(values)}
+      onSubmit={(values) => {
+        console.log(values)
+        console.log("Your post has been uploaded!")
+        navigation.goBack()
+      }}
       validationSchema={uploadPostSchema}
       validateOnMount={true}
     >
