dd7be68e4eb6f5969d402241075657f1f9d3ea22
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-04-16 14:38:16 +0200

    Read posts from firebase db

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,22 @@
-import React from "react"
 import { StyleSheet, ScrollView } from "react-native"
 import SafeAreaView from "react-native-safe-area-view"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import Header from "../components/Home/Header"
 import Post from "../components/Home/Post"
 import Stories from "../components/Home/Stories"
+import React, { useEffect } from "react"
 import BottomTabs from "../components/Home/BottomTabs"
 import { POSTS } from "../data/posts"
 import { bottomTabIcons } from "../data/bottomTabIcons"
+import { db } from "../firebase"
+import { onSnapshot, collectionGroup } from "firebase/firestore"
 
 const HomeScreen = ({ navigation }) => {
+  useEffect(() => {
+    onSnapshot(collectionGroup(db, "posts"), (querySnapshot) => {
+      console.log(querySnapshot.docs.map((doc) => doc.data()))
+    })
+  }, [])
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
