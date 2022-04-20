7ef3228c93f7745b24bbf5ab4eda872a7a9881d6
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-04-20 10:58:29 +0200

    Spacing between stories

--- components/Home/Stories.js
+++ components/Home/Stories.js
@@ -11,7 +11,7 @@ const Stories = () => {
             <Image source={{ uri: story.image }} style={styles.story} />
             <Text style={{ color: "white" }}>
               {story.user.length > 11
-                ? story.user.slice(0, 10).toLowerCase() + " ... "
+                ? story.user.slice(0, 6).toLowerCase() + " ... "
                 : story.user.toLowerCase()}
             </Text>
           </View>
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     width: 70,
     height: 70,
     borderRadius: 50,
-    marginLeft: 6,
+    marginLeft: 18,
     borderWidth: 3,
     borderColor: "#ff8501",
   },
